2ce6ca999df6b0912cc75c9f81449d718b255068
Vladislav Suvalov <vladislav.suvalov@hp.com>
2023-04-04 21:26:24 +0300

    add: footer grid

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
+import instaOK from '../img/instaOK.png';
+
 function Footer() {
     return (
         <footer className="footer">
         <nav>
-        <h2>Doormat Navigation</h2>
-        <ul>
+        <h2 className="doormat-title">Doormat Navigation</h2>
+        <ul className="doormat-nav">
             <li><a href="/">Home</a></li>
             <li><a href="/about">About</a></li>
             <li><a href="/menu">Menu</a></li>
@@ -12,16 +14,20 @@ function Footer() {
             <li><a href="/login">Login</a></li>
         </ul>
         </nav>
-        <div>
+        <div className="contact">
             <h2>Contact</h2>
-            <p>Adress</p>
-            <p>phone number</p>
-            <p>email</p>
+            <div className="info">
+            <p>Little Street 42, Tallinn</p>
+            <p>+3725304125</p>
+            <p>little.lemon@booking.com</p>
+            </div>
         </div>
-        <div>
+        <div className="">
             <h2>Social Media Links</h2>
-            <a href='/facebook'>Facebook<img src='' alt='facebook'></img></a>
-            <a href='/instagram'>Instagram<img src='' alt='instagram'></img></a>
+            <div className="">
+            <a href='/facebook'><img className='fb' src='https://cdn3.iconfinder.com/data/icons/picons-social/57/46-facebook-512.png' alt='facebook'></img></a>
+            <a href='/instagram'><img className="insta" src={instaOK} alt='instagram'></img></a>
+            </div>
         </div>
         </footer>
     )
