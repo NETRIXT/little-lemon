0945ae3e57809922cc1d6797bd56eb1e6803aa15
Vladislav Suvalov <vladislav.suvalov@hp.com>
2023-04-05 21:59:15 +0300

    add: little lemon intro section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,22 @@
 function Main() {
     return (
-        <main>
+        <main className='main'>
+         <section className="intro-section">
+            <div className='left-section'>
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>Laborum amet deserunt ex consequat id quis culpa duis officia nostrud eiusmod nisi.
+            Ullamco officia incididunt quis elit. Cupidatat mollit duis mollit Lorem aliquip in.
+            Non elit qui eu exercitation aliqua duis non elit proident duis.</p>
+
+        <button>Reserve a Table</button>
+        </div>
+
+        <div className='right-section'>
+            <img src={'logo'} alt='logo'></img>
+        </div>
+
+         </section>
 
         </main>
     )
